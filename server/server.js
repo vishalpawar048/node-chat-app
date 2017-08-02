a071893abcc3d6919bc2651c844bb078fa041f27
vishalpawar048 <vishalpawar048@gmail.com>
2017-08-02 12:50:20 +0530

    greet new user

--- server/server.js
+++ server/server.js
@@ -14,10 +14,27 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage',{
+    from:'Admin',
+    text:'Wellcome',
+    createdAt:new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage',{
+    from:'Admin',
+    text:'new user joined',
+    createdAt:new Date().getTime()
+  });
 
   socket.on('createMessage',(message)=>{
     console.log(message);
     io.emit('newMessage',{
+      from:message.from,
+      text:message.text,
+      createdAt:new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage',{
       from:message.from,
       text:message.text,
       createdAt:new Date().getTime()
