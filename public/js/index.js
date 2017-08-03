82a5327b93b9f2422a74f197fc082de8c3d26426
vishalpawar048 <vishalpawar048@gmail.com>
2017-08-03 20:08:31 +0530

    tempelets

--- public/js/index.js
+++ public/js/index.js
@@ -9,25 +9,29 @@
     });
 
 
-    socket.on('newMessage',function(message){
-        console.log('message',message);
-        var formettedTime=moment().format('h:mm a');
-        var li=jQuery('<li></li>');
-        li.text(`${message.from}:${formettedTime}:${message.text}`);
+    socket.on('newMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-        jQuery('#messages').append(li);
-    });
+  jQuery('#messages').append(html);
+});
 
-     socket.on('newLocationMessage',function(message){
-         var formettedTime=moment().format('h:mm a');
-        var li=jQuery('<li></li>');
-        var a=jQuery('<a target="_blank">My Location</a>');
-        li.text(`${message.from}:${formettedTime}`);
-        a.attr('href',message.url);
-        li.append(a);
-        jQuery('#messages').append(li);
-    });
+socket.on('newLocationMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
 
+  jQuery('#messages').append(html);
+});
 
     jQuery('#message-form').on('submit',function(e){
         e.preventDefault();
